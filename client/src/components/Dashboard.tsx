c647e8265b8e372020231b50c0c533fb57502277
Yuze Fu <git@xfox.me>
2018-01-30 12:40:08 +0800

    client: add shadowsocks config

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -13,151 +13,151 @@ import Tabs, { Tab } from 'material-ui/Tabs';
 import TextField from 'material-ui/TextField/TextField';
 
 import theme from '../lib/theme';
+import { GuideItem } from '../lib/guide';
 
 @inject('state') @observer
 class Dashboard extends React.Component<RouteComponentProps<{}> & { state: AppState }, {
-  currentStepShared: number;
-  currentOsShared: number;
+  currentStep: number[];
+  currentOs: number[];
 }> {
   state = {
-    currentStepShared: 0,
-    currentOsShared: 0,
+    currentStep: [0, 0],
+    currentOs: [0, 0],
   };
 
-  handleChangeTab = (event: ChangeEvent<{}>, value: number) => {
-    this.setState({
-      currentStepShared: 0,
-      currentOsShared: value
-    });
+  handleChangeTab = (index: number) => (event: ChangeEvent<{}>, value: number) => {
+    this.setState((state) => ({
+      currentStep: state.currentStep.map((v, i) => i === index ? 0 : v),
+      currentOs: state.currentOs.map((v, i) => i === index ? value : v)
+    }));
   }
-  handleNext = () => {
-    this.setState({
-      currentStepShared: this.state.currentStepShared + 1,
-    });
+  handleNext = (index: number) => () => {
+    this.setState((state) => ({
+      currentStep: state.currentStep.map((v, i) => i === index ? (v + 1) : v)
+    }));
   }
-  handleBack = () => {
-    this.setState({
-      currentStepShared: this.state.currentStepShared - 1,
-    });
+  handleBack = (index: number) => () => {
+    this.setState((state) => ({
+      currentStep: state.currentStep.map((v, i) => i === index ? (v - 1) : v)
+    }));
   }
-  handleReset = () => {
-    this.setState({
-      currentStepShared: 0,
-    });
+  handleReset = (index: number) => () => {
+    this.setState((state) => ({
+      currentStep: state.currentStep.map((v, i) => i === index ? 0 : v),
+    }));
   }
 
   render() {
     const guide = this.props.state.guide;
-    return (
-      <span>
-        {guide &&
-          <Card style={{ marginTop: '10px' }}>
-            <CardContent>
-              <Typography type="headline" component="h2">
-                VMess 配置说明（推荐）
-            </Typography>
-              <Typography>
-                <Tabs
-                  value={this.state.currentOsShared}
-                  onChange={this.handleChangeTab}
-                  indicatorColor="secondary"
-                  textColor="secondary"
-                  fullWidth
-                  scrollable
-                  scrollButtons="auto"
-                >
-                  {guide.vmess.map((value) =>
-                    <Tab label={value.os} />
-                  )}
-                </Tabs>
-              </Typography>
-              <Stepper activeStep={this.state.currentStepShared} orientation="vertical">
-                {guide.vmess[this.state.currentOsShared].descriptions.map((description, index) =>
-                  <Step key={description.title}>
-                    <StepLabel>{description.title}</StepLabel>
-                    <StepContent>
-                      <Typography>
-                        <p>{description.content}</p>
-                        {
-                          description.links &&
-                          <p>
-                            {description.links.map((link) => (
-                              <span style={{ paddingRight: '4px' }}>
-                                <Button raised color="secondary" href={link.href} target="_blank">
-                                  {link.name}
-                                </Button>
-                              </span>
-                            ))}
-                          </p>
-                        }
-                        {
-                          description.fields &&
-                          <p>
-                            {description.fields.map((value) =>
-                              <TextField
-                                label={value.name}
-                                value={value.data}
-                                margin="normal"
-                                fullWidth
-                                disabled
-                              />
-                            )}
-                          </p>
-                        }
-                        {
-                          description.qrcode &&
-                          <p>
-                            <img src={description.qrcode} />
-                          </p>
-                        }
-                        {
-                          description.code &&
+    const makeGuideUi = (title: string, guide: GuideItem[], index: number) => (
+      <Card style={{ marginTop: '10px' }}>
+        <CardContent>
+          <Typography type="headline" component="h2">
+            {title}
+          </Typography>
+          <Typography>
+            <Tabs
+              value={this.state.currentOs[index]}
+              onChange={this.handleChangeTab(index)}
+              indicatorColor="secondary"
+              textColor="secondary"
+              fullWidth
+              scrollable
+              scrollButtons="auto"
+            >
+              {guide.map((value) =>
+                <Tab label={value.os} />
+              )}
+            </Tabs>
+          </Typography>
+          <Stepper activeStep={this.state.currentStep[index]} orientation="vertical">
+            {guide[this.state.currentOs[index]].descriptions.map((description) =>
+              <Step key={description.title}>
+                <StepLabel>{description.title}</StepLabel>
+                <StepContent>
+                  <Typography>
+                    <p>{description.content}</p>
+                    {
+                      description.links &&
+                      <p>
+                        {description.links.map((link) => (
+                          <span style={{ paddingRight: '4px' }}>
+                            <Button raised color="secondary" href={link.href} target="_blank">
+                              {link.name}
+                            </Button>
+                          </span>
+                        ))}
+                      </p>
+                    }
+                    {
+                      description.fields &&
+                      <p>
+                        {description.fields.map((value) =>
                           <TextField
-                            label={description.code.name}
                             multiline
-                            value={description.code.data}
+                            label={value.name}
+                            value={value.data}
                             margin="normal"
                             fullWidth
                             disabled
                           />
-                        }
-                      </Typography>
-                      <Button
-                        disabled={this.state.currentStepShared === 0}
-                        onClick={this.handleBack}
-                        style={{ margin: theme.spacing.unit }}
-                      >
-                        后退
+                        )}
+                      </p>
+                    }
+                    {
+                      description.qrcode &&
+                      <p>
+                        <img src={description.qrcode} />
+                      </p>
+                    }
+                  </Typography>
+                  <Button
+                    disabled={this.state.currentStep[index] === 0}
+                    onClick={this.handleBack(index)}
+                    style={{ margin: theme.spacing.unit }}
+                  >
+                    后退
                       </Button>
-                      <Button
-                        disabled={this.state.currentStepShared ===
-                          guide.vmess[this.state.currentOsShared].descriptions.length}
-                        raised
-                        color="primary"
-                        onClick={this.handleNext}
-                        style={{ margin: theme.spacing.unit }}
-                      >
-                        {this.state.currentStepShared !==
-                          guide.vmess[this.state.currentOsShared].descriptions.length - 1 ?
-                          '前进' : '完成'}
-                      </Button>
-                    </StepContent>
-                  </Step>
-                )}
-              </Stepper>
-              {
-                this.state.currentStepShared ===
-                guide.vmess[this.state.currentOsShared].descriptions.length &&
-                <Button
-                  raised
-                  color="primary"
-                  onClick={this.handleReset}
-                >
-                  重置
+                  <Button
+                    {...console.log(this.state, index, guide) }
+                    disabled={this.state.currentStep[index] ===
+                      guide[this.state.currentOs[index]].descriptions.length}
+                    raised
+                    color="primary"
+                    onClick={this.handleNext(index)}
+                    style={{ margin: theme.spacing.unit }}
+                  >
+                    {this.state.currentStep[index] !==
+                      guide[this.state.currentOs[index]].descriptions.length - 1 ?
+                      '前进' : '完成'}
+                  </Button>
+                </StepContent>
+              </Step>
+            )}
+          </Stepper>
+          {
+            this.state.currentStep[index] ===
+            guide[this.state.currentOs[index]].descriptions.length &&
+            <Button
+              raised
+              color="primary"
+              onClick={this.handleReset(index)}
+            >
+              重置
                 </Button>
-              }
-            </CardContent>
-          </Card>}
+          }
+        </CardContent>
+      </Card>
+    );
+    return (
+      <span>
+        {guide &&
+          <div>
+            {[
+              makeGuideUi("VMess 配置说明（推荐）", guide.vmess, 0),
+              makeGuideUi("Shadowsocks 配置说明", guide.ss, 1),
+            ]}
+          </div>}
         <Announces state={this.props.state} count={2} />
       </span>
     );
